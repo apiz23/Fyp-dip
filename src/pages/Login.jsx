d9640890320e8facaa98bf31f5a9aca0ed727a5a
Hafizuddin <piz230601@gmail.com>
2023-06-24 10:33:32 +0800

    adding the loader component in login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,6 +12,14 @@ export default function Login() {
 	const [errors, setErrors] = useState({});
 	const [values, setValues] = useState({ username: "", password: "" });
 
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 2000);
+	}, []);
+
 	useEffect(() => {
 		const getLogin = async () => {
 			const data = await getDocs(collection(db, "login"));
@@ -87,110 +95,114 @@ export default function Login() {
 
 	return (
 		<section className="loginSec">
-			<div>
-				<div className="container py-5 h-100">
-					<div className="row d-flex justify-content-center align-items-center">
-						<div className="col-12 col-md-8 col-lg-6 col-xl-5">
-							<div className="card">
-								<div className="card-body p-5 text-center">
-									<img src={logo} width={70} />
-									<h3 className="mt-4">Space & Equipment</h3>
-									<form onSubmit={handleSubmit}>
-										<div className="form-outline my-3">
+			{isLoading ? (
+				<div className="divLoader">
+					<div
+						className="spinner spinner-border text-light"
+						role="status"
+						style={{ width: "5rem", height: "5rem" }}
+					>
+						<span className="visually-hidden">Loading...</span>
+					</div>
+				</div>
+			) : null}
+
+			<div className="container py-5 h-100">
+				<div className="row d-flex justify-content-center align-items-center">
+					<div className="col-12 col-md-8 col-lg-6 col-xl-5">
+						<div className="card">
+							<div className="card-body p-5 text-center">
+								<img src={logo} width={70} />
+								<h3 className="mt-4">Space & Equipment</h3>
+								<form onSubmit={handleSubmit}>
+									<div className="form-outline my-3">
+										<input
+											type="text"
+											id="username"
+											className="form__input"
+											placeholder="Username"
+											name="username"
+											value={values.username}
+											onChange={handleChange}
+										/>
+										{errors.username && (
+											<div
+												className="alert alert-danger alert-dismissible fade show"
+												role="alert"
+											>
+												<span>{errors.username}</span>
+											</div>
+										)}
+									</div>
+
+									<div className="form-outline">
+										<input
+											type="password"
+											name="password"
+											id="password"
+											className="form__input"
+											placeholder="Password"
+											value={values.password}
+											onChange={handleChange}
+										/>
+										{errors.password && (
+											<div
+												className="alert alert-danger alert-dismissible fade show"
+												role="alert"
+											>
+												<span>{errors.password}</span>
+											</div>
+										)}
+									</div>
+									<div className="radio-row">
+										<div className="form-check form-check-inline">
 											<input
-												type="text"
-												id="username"
-												className="form__input"
-												placeholder="Username"
-												name="username"
-												value={values.username}
-												onChange={handleChange}
+												className="form-check-input"
+												type="radio"
+												name="flexRadioDefault"
+												id="inlineCheckbox1"
+												value="stdStaff"
+												checked={radioCheck === "stdStaff"}
+												onChange={handleRadioChange}
 											/>
-											{errors.username && (
-												<div
-													className="alert alert-danger alert-dismissible fade show"
-													role="alert"
-												>
-													<span>{errors.username}</span>
-												</div>
-											)}
+											<label className="form-check-label" for="inlineCheckbox1">
+												Student / Staff
+											</label>
 										</div>
-
-										<div className="form-outline">
+										<div className="form-check form-check-inline">
 											<input
-												type="password"
-												name="password"
-												id="password"
-												className="form__input"
-												placeholder="Password"
-												value={values.password}
-												onChange={handleChange}
+												className="form-check-input"
+												type="radio"
+												name="flexRadioDefault"
+												id="inlineCheckbox2"
+												value="admin"
+												checked={radioCheck === "admin"}
+												onChange={handleRadioChange}
 											/>
-											{errors.password && (
-												<div
-													className="alert alert-danger alert-dismissible fade show"
-													role="alert"
-												>
-													<span>{errors.password}</span>
-												</div>
-											)}
+											<label className="form-check-label" for="inlineCheckbox1">
+												Admin
+											</label>
 										</div>
-										<div className="radio-row">
-											<div className="form-check form-check-inline">
-												<input
-													className="form-check-input"
-													type="radio"
-													name="flexRadioDefault"
-													id="inlineCheckbox1"
-													value="stdStaff"
-													checked={radioCheck === "stdStaff"}
-													onChange={handleRadioChange}
-												/>
-												<label
-													className="form-check-label"
-													for="inlineCheckbox1"
-												>
-													Student / Staff
-												</label>
-											</div>
-											<div className="form-check form-check-inline">
-												<input
-													className="form-check-input"
-													type="radio"
-													name="flexRadioDefault"
-													id="inlineCheckbox2"
-													value="admin"
-													checked={radioCheck === "admin"}
-													onChange={handleRadioChange}
-												/>
-												<label
-													className="form-check-label"
-													for="inlineCheckbox1"
-												>
-													Admin
-												</label>
-											</div>
+									</div>
+
+									<hr className="my-2" />
+
+									<div className="row d-flex justify-content-center">
+										<div className="col-lg">
+											<input
+												type="submit"
+												value="Submit"
+												className="btnSubmit btn-primary"
+											/>
 										</div>
+									</div>
 
-										<hr className="my-2" />
-
-										<div className="row d-flex justify-content-center">
-											<div className="col-lg">
-												<input
-													type="submit"
-													value="Submit"
-													className="btnSubmit btn-primary"
-												/>
-											</div>
-										</div>
-
-										<p className="mt-3 text-primary-emphasis">
-											Only <strong>UTHM students</strong> with active status are
-											allowed to login to this system. For any enquiries, please
-											click here to <strong>contact us</strong>
-										</p>
-									</form>
-								</div>
+									<p className="mt-3 text-primary-emphasis">
+										Only <strong>UTHM students</strong> with active status are
+										allowed to login to this system. For any enquiries, please
+										click here to <strong>contact us</strong>
+									</p>
+								</form>
 							</div>
 						</div>
 					</div>
